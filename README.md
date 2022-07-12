7a98c91413b50c9d34e7cad2b90201e018f1b938
Aksiiii <zaloznikos@seznam.cz>
2022-07-12 04:32:10 +0200

    edited code to assist fixing terminal issue

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Engeto_Project3
+
+https://volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=7&xnumnuts=5103
--- election_scraper.py
+++ election_scraper.py
@@ -1,4 +1,27 @@
-print("hello world")
-
+import sys
 import requests
 from bs4 import BeautifulSoup
+
+url = "https://volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=7&xnumnuts=5103"
+
+try:
+    response = requests.get(str(sys.argv[0]))
+    # response = requests.get(url)
+except requests.exceptions.InvalidSchema:
+    print("Invalid URL")
+    while True:
+        test = input("type letter A")
+        if test == "a":
+            quit()
+else:
+    soup = BeautifulSoup(response.text, "html.parser")
+
+elect = soup.find("div", {"class": "topline"})
+print(elect.prettify())
+
+# print(len(sys.argv), str(sys.argv))
+
+while True:
+    test = input("type letter A")
+    if test == "a":
+        break
