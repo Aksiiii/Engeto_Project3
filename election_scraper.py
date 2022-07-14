aac197b593c808017574b2bafbe42f5fdbd5baa3
Aksiiii <zaloznikos@seznam.cz>
2022-07-14 22:21:30 +0200

    added function to get vote count, and combine it with regions code and name

--- election_scraper.py
+++ election_scraper.py
@@ -27,22 +27,52 @@ def clean_url(urls):
 
 
 def sub_url_opener(link):
-    # opens provided url from provided variable
+    # opens provided url from and picks the desired parent
     res = requests.get(link[0])
     sub_soup = BeautifulSoup(res.text, "html.parser")
-    sub_html = sub_soup.find("div", id="inner")
+    sub_html = sub_soup.find("div", id="publikace")
     return sub_html
 
 
 def fields_line(link):
-    # prepares the fields for final .csv file
+    # prepares fields for final .csv file
     fields = ["Code", "Region", "Registered", "Envelopes", "Valid"]
     party_list = []
-    for party in link.findAll("td", {"class": "overflow_name"}):
+    for party in link.find_all("td", {"class": "overflow_name"}):
         party_list.append(party.get_text())
     fields.extend(party_list)
     return fields
 
+
+def voter_info(sub_html):
+    cells = []
+    for cell in sub_html.find_all("td", {"data-rel": "L1"}):
+        cells.append(cell.get_text())
+    cells.remove(cells[2])
+    return cells
+
+
+def party_votes(sub_html):
+    tag = "t1sa2 t1sb3"
+    switch = 0
+    votes = []
+    while switch < 2:
+        for vote in sub_html.find_all("td", headers=tag):
+            votes.append(vote.get_text())
+        tag = tag.replace("1", "2")
+        switch += 1
+    return votes
+
+
+def row_combiner(cod, reg, lin):
+    row = [cod[0], reg[0]]
+    row.extend(voter_info(sub_url_opener(lin)))
+    row.extend(party_votes(sub_url_opener(lin)))
+
+    return row
+
+
+rows = []
 regions = []
 code = []
 links = []
@@ -64,22 +94,25 @@ else:
     soup = BeautifulSoup(response.text, "html.parser")
 tables = soup.find("div", id="inner")
 
-for group in tables.findAll("a"):
+for group in tables.find_all("a"):
     code.append(group.get_text())
     links.append(group.get("href"))
 links = clean_url(links)
 
-for name in tables.findAll("td", {"class": "overflow_name"}):
+for name in tables.find_all("td", {"class": "overflow_name"}):
     regions.append(name.get_text())
 
-print(code)
-print(regions)
+# print(code)
+# print(regions)
 print(fields_line(sub_url_opener(links)))
-
+# print(voter_info(sub_url_opener(links)))
+# print(party_votes(sub_url_opener(links)))
+print(row_combiner(code, regions, links))
 # print(soup.prettify())
 #  for row in elect.find_all("a"):
 #      rows.append(row.text)
-
-# with open(f_name, 'w') as f:
-#     write = csv.writer(f)
-#     write.writerow(fields_line(sub_url_opener(links)))
+test = [row_combiner(code, regions, links), row_combiner(code, regions, links)]
+with open(f_name, "w", newline='\n') as f:
+    write = csv.writer(f)
+    write.writerow(fields_line(sub_url_opener(links)))
+    write.writerows(test)
